3f00e0e9b46c9cbd8406a778430afb9de49f53a9
Ben <quillaja@hotmail.com>
2018-02-21 01:45:26 -0800

    i think i got MouseCamera working exactly how i want it.

--- pxu/mouseCamera.go
+++ pxu/mouseCamera.go
@@ -12,36 +12,50 @@ import (
 // editing software. Zooming is done such that the "zoom locus" is the point
 // the mouse is currently over.
 //
-// "if I zoom with the mouse at the top right, then the same "world coordinates"
-// would be under the mouse after the zoom."
+// For a basic example of how to use the camera, see the `Camera` interface,
+// and read more in the documentation for `KeyCamera`.
 type MouseCamera struct {
-	Position  pixel.Vec
-	Zoom      float64
-	ZoomSpeed float64
+	Position  pixel.Vec // the 'world' location of the camera
+	Zoom      float64   // the zoom factor of the camera
+	ZoomSpeed float64   // how quickly the camera zooms. should be >1
 
-	XExtents clamp
-	YExtents clamp
-	ZExtents clamp
+	XExtents clamp // min and max extents the camera can pan horizontally
+	YExtents clamp // min and max extents the camera can pan vertically
+	ZExtents clamp // min and max extents for the zoom factor
 
-	DragButton pixelgl.Button
+	DragButton pixelgl.Button // the button which performs drag when pressed and held. default is left mouse buttom
 
-	viewMatrix   pixel.Matrix
-	prevMousePos pixel.Vec
-	origCenter   pixel.Vec
+	// used internally
+	viewMatrix        pixel.Matrix
+	worldZeroInWindow pixel.Vec
+	prevMousePos      pixel.Vec
+	origPos           pixel.Vec
+	origZoom          float64
 }
 
-func NewMouseCamera(initialCenter pixel.Vec) *MouseCamera {
+func NewMouseCamera(worldZeroInWindow pixel.Vec) *MouseCamera {
 	return &MouseCamera{
-		Position:     initialCenter,
-		Zoom:         1,
-		ZoomSpeed:    1.1,
-		XExtents:     clamp{-5000, 5000},
-		YExtents:     clamp{-5000, 5000},
-		ZExtents:     clamp{-50, 50},
-		DragButton:   pixelgl.MouseButtonLeft,
-		viewMatrix:   pixel.IM.Moved(initialCenter),
-		prevMousePos: pixel.ZV,
-		origCenter:   initialCenter}
+		Position:          pixel.ZV,
+		Zoom:              1,
+		ZoomSpeed:         1.1,
+		XExtents:          clamp{-5000, 5000},
+		YExtents:          clamp{-5000, 5000},
+		ZExtents:          clamp{-50, 50},
+		DragButton:        pixelgl.MouseButtonLeft,
+		viewMatrix:        pixel.IM.Moved(worldZeroInWindow),
+		worldZeroInWindow: worldZeroInWindow,
+		prevMousePos:      pixel.ZV,
+		origPos:           pixel.ZV,
+		origZoom:          1}
+}
+
+func NewMouseCameraParams(worldZeroInWindow, origPos pixel.Vec, origZoom, zoomSpeed float64) *MouseCamera {
+	c := NewMouseCamera(worldZeroInWindow)
+	c.origPos = origPos
+	c.Zoom = origZoom
+	c.ZoomSpeed = zoomSpeed
+	c.origZoom = origZoom
+	return c
 }
 
 func (c *MouseCamera) Update(win *pixelgl.Window) {
@@ -51,14 +65,18 @@ func (c *MouseCamera) Update(win *pixelgl.Window) {
 		c.prevMousePos = win.MousePosition()
 	} else if win.Pressed(pixelgl.MouseButtonLeft) {
 		newMousePos := win.MousePosition()
-		delta := newMousePos.Sub(c.prevMousePos)
+		delta := newMousePos.Sub(c.prevMousePos).Scaled(-1) // this delta is in the opposite direction we want to move "Position", so invert
+		c.prevMousePos = newMousePos                        // todo? i wonder if there's a potential error here
 
-		// todo: check that Pos + delta is within extents.
-		// if NOT, then alter delta to land in extents, update Pos etc.
-		c.Position = c.Position.Add(delta)
-		c.prevMousePos = newMousePos
+		// check that new position is within extents.
+		// if NOT then clamp
+		newPos := c.Position.Add(delta.Scaled(1 / c.Zoom))
+		newPos.X = pixel.Clamp(newPos.X, c.XExtents.Low, c.XExtents.High)
+		newPos.Y = pixel.Clamp(newPos.Y, c.YExtents.Low, c.YExtents.High)
 
-		c.viewMatrix = c.viewMatrix.Moved(delta)
+		// update matrix and position
+		c.viewMatrix = c.viewMatrix.Moved(newPos.Sub(c.Position).Scaled(-c.Zoom))
+		c.Position = newPos
 	}
 
 	// scale the matrix only when the mouse wheel is scrolled. MouseScroll()
@@ -66,11 +84,20 @@ func (c *MouseCamera) Update(win *pixelgl.Window) {
 	// the change in zoom
 	if win.MouseScroll().Y != 0 {
 		delta := math.Pow(c.ZoomSpeed, win.MouseScroll().Y)
-		//TODO: check that Zoom*delta is within extents.
-		// if NOT, then alter delta to land in extents, update Zoom, etc.
-		c.Zoom *= delta
-		c.viewMatrix = c.viewMatrix.Scaled(win.MousePosition(), delta)
+		// check that the new zoom is within extents.
+		// if NOT then clamp
+		newZoom := c.Zoom * delta
+		newZoom = pixel.Clamp(newZoom, c.ZExtents.Low, c.ZExtents.High)
+
+		// update matrix and zoom
+		c.viewMatrix = c.viewMatrix.Scaled(win.MousePosition(), newZoom/c.Zoom)
+		c.Zoom = newZoom
+
+		// move position so that point under window center
+		// accurately reflects "world" coordinate
+		c.Position = c.viewMatrix.Unproject(win.Bounds().Center())
 	}
+
 }
 
 func (c *MouseCamera) GetMatrix() pixel.Matrix {
@@ -78,42 +105,21 @@ func (c *MouseCamera) GetMatrix() pixel.Matrix {
 }
 
 func (c *MouseCamera) ResetPosition() {
-	c.viewMatrix = c.viewMatrix.Moved(c.origCenter.Sub(c.Position))
-	c.Position = c.origCenter
+	c.viewMatrix = pixel.IM.Scaled(pixel.ZV, c.Zoom).Moved(c.worldZeroInWindow)
+	c.Position = c.origPos
 }
 
 func (c *MouseCamera) ResetZoom() {
-	c.viewMatrix = pixel.IM.Moved(c.Position)
-	c.Zoom = 1
+	c.viewMatrix = pixel.IM.Moved(c.worldZeroInWindow.Sub(c.Position))
+	c.Zoom = c.origZoom
 }
 
 func (c *MouseCamera) Reset() {
-	c.ResetPosition()
+	// could do c.viewMatrix = pixel.IM.Moved(c.worldZeroInWindow) as well
 	c.ResetZoom()
+	c.ResetPosition()
 }
 
-/*
-@elliotmr's original code.
-
-cameraOrigin := pixel.ZV.Add(win.Bounds().Center())
-scale := 1.0
-dragOrigin := pixel.V(0, 0)
-second := time.Tick(time.Second)
-viewMatrix := pixel.IM
-frames := 0
-for !win.Closed() {
-	if win.MouseScroll().Y != 0 {
-		factor := math.Pow(1.2, win.MouseScroll().Y)
-		zoomDeltaStart := viewMatrix.Unproject(win.MousePosition())
-		scale *= factor
-		cameraOrigin = zoomDeltaStart.Add(win.Bounds().Center().Sub(win.MousePosition().Scaled(1 / scale)))
-	}
-	if win.JustPressed(pixelgl.MouseButton1) {
-		dragOrigin = win.MousePosition().Scaled(1 / scale)
-	} else if win.Pressed(pixelgl.MouseButton1) {
-		newOrigin := win.MousePosition().Scaled(1 / scale)
-		cameraOrigin = cameraOrigin.Sub(newOrigin.Sub(dragOrigin))
-		dragOrigin = newOrigin
-	}
-	viewMatrix = pixel.IM.Moved(win.Bounds().Center().Sub(cameraOrigin)).Scaled(pixel.ZV, scale)
-*/
+func (c *MouseCamera) Unproject(point pixel.Vec) pixel.Vec {
+	return c.viewMatrix.Unproject(point)
+}
